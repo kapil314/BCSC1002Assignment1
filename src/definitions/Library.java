f3f067764d3cb0aeb2744bb2e0af5dc9cef03c85
KAPIL JADAUN <kapil.jadaun_cs19@gla.ac.in>
2020-08-30 21:59:15 +0530

    create field and getter , setter method for this field

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,10 +1,20 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
+ *  User: Kapil Jadaun
+ *  Date: 30/08/20
+ *  Time: 21:52
  *  File Name : Library.java
  * */
 package definitions;
 
 public class Library {
+    private Book[] currentlyAvailableBooks;
+
+    public Book[] getCurrentlyAvailableBooks() {
+        return currentlyAvailableBooks;
+    }
+
+    public void setCurrentlyAvailableBooks(Book[] currentlyAvailableBooks) {
+        this.currentlyAvailableBooks = currentlyAvailableBooks;
+    }
+
 }
